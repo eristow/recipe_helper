42f4bca37d4ea680d3e9437aed988d2a2f6588ba
Evan Ristow <evan.ristow1@gmail.com>
2024-09-13 20:00:16 -0400

    Initial connection to psql db. TODO: use env var instead of hard-coded DB URL

--- backend/cmd/service/main.go
+++ backend/cmd/service/main.go
@@ -1,15 +1,18 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"strings"
 
 	"github.com/eristow/recipe_helper_backend/internal/database"
 	"github.com/eristow/recipe_helper_backend/internal/recipe"
 	"github.com/eristow/recipe_helper_backend/internal/rest"
 	"github.com/eristow/recipe_helper_backend/internal/util"
+	"github.com/jackc/pgx/v5"
 	"github.com/ollama/ollama/api"
 )
 
@@ -50,6 +53,50 @@ func main() {
 
 	ds.AddRecipe(pancakeRecipe)
 	ds.AddRecipe(pizzaRecipe)
+
+	// Add test entry into DB
+	// TODO: get DB URL from env
+	// databaseUrl := os.Getenv("DATABASE_URL")
+	databaseUrl := "postgres://defaultuser:defaultpassword@database:5432/defaultdatabase"
+	log.Println("Database URL: ", databaseUrl)
+	conn, err := pgx.Connect(context.Background(), databaseUrl)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close(context.Background())
+
+	_, err = conn.Exec(context.Background(), "CREATE TABLE IF NOT EXISTS recipes (id uuid DEFAULT gen_random_uuid(), name TEXT, ingredients TEXT[], steps TEXT[], PRIMARY KEY (id))")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to create table: %v\n", err)
+		os.Exit(1)
+	}
+
+	_, err = conn.Exec(context.Background(), "INSERT INTO recipes (name, ingredients, steps) VALUES ($1, $2, $3)", "Pancakes", []string{"Flour", "Eggs", "Milk", "Sugar"}, []string{"Mix ingredients", "Cook on pan"})
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to create table: %v\n", err)
+		os.Exit(1)
+	}
+
+	// Test getting from DB
+	rows, err := conn.Query(context.Background(), "SELECT name, ingredients, steps FROM recipes")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to get recipes from table: %v\n", err)
+		os.Exit(1)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var name string
+		var ingredients []string
+		var steps []string
+		err = rows.Scan(&name, &ingredients, &steps)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Unable to parse recipe: %v\n", err)
+			os.Exit(1)
+		}
+		fmt.Println(name, ingredients, steps)
+	}
 
 	mux.Handle("/", util.HandleCors(rootH))
 	mux.Handle("/recipes/", util.HandleCors(recipeH))
